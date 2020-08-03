e756a6f5d99223c5fc4672fb6b6e1220fd0e4ac7
Navid Khalili <nkhalili@live.com>
2020-08-02 22:13:59 -0400

    init

---
+++ src/main/java/com/navid/roomcli/RoomCliApplication.java
@@ -0,0 +1,13 @@
+package com.navid.roomcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomCliApplication.class, args);
+    }
+
+}
